4e783f05c2f6b772fb45741f84abd949eaec94eb
many bear <98581612+6666dustry@users.noreply.github.com>
2025-05-25 09:46:17 +0900

    boostを使えるように変更

--- .vscode/acl/atcoder-c++/main.cpp
+++ .vscode/acl/atcoder-c++/main.cpp
@@ -7,6 +7,18 @@
 #include <atcoder/all>
 #endif
 #include <bits/stdc++.h>
+// # include <boost/multiprecision/cpp_dec_float.hpp>
+// # include <boost/multiprecision/cpp_int.hpp>
+// # include <boost/rational.hpp>
+// namespace mp = boost::multiprecision;
+// // 任意長整数型
+// using Bint = mp::cpp_int;
+// // 仮数部が32桁(10進数)の浮動小数点数型
+// using Real32 = mp::number<mp::cpp_dec_float<32>>;
+// // 仮数部が1024桁(10進数)の浮動小数点数型
+// using Real1024 = mp::number<mp::cpp_dec_float<1024>>;
+// // ついでに有理数型
+// using Rat = boost::rational<Bint>;
 using namespace atcoder;
 
 using namespace std;
