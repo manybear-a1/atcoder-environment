c4be7d7c82b690c5703ec0b9a7cd69aa4dcad7a6
many bear <98581612+6666dustry@users.noreply.github.com>
2025-06-21 10:07:14 +0900

    更新忘れ

--- .vscode/ahc-template/forjava.ps1
+++ .vscode/ahc-template/forjava.ps1
@@ -1,10 +1,19 @@
 # compile all Java sources into Java 8 bytecode (class version 52.0)
-# change location
+# https://atcoder.jp/contests/rco-contest-2019-qual/tasks/rco_contest_2019_qual_a
 Set-Location .\2025\6\rco_contest_2019_qual_a\tools
-javac --release 8 *.java
+chcp 65001
+javac -encoding UTF-8 Generator.java --release 8
+javac -encoding UTF-8 Judge.java --release 8
 # generate test case with seed $i (four digits like 0000, 0001, ..., 0099)
 for ($i = 0; $i -lt 100; $i++) {
   $i_ = "{0:D4}" -f $i
   Write-Host "Generating test case with seed $i"
-  java Generator -seed $i_ > in/$i_.txt
+  $input = java Generator -Dfile.encoding=UTF-8 -seed $i_ # > in/$i_.txt
+  # change encoding to UTF-8 (no BOM)
+  # write UTF-8 without BOM via .NET API, preserving newlines
+  [System.IO.File]::WriteAllLines(
+    "D:\atcoder\2025\6\rco_contest_2019_qual_a\tools\in\$i_.txt",
+    $input,
+    (New-Object System.Text.UTF8Encoding($false))
+  )
 }
